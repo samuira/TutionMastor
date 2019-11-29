aa785c17b06b34e6593d6016a46fc4e90f2b753b
DAT-ASSET-36 <rajesh.samui@digitalaptech.com>
2019-11-29 19:51:24 +0530

    celery implementation

--- WorkForce/settings.py
+++ WorkForce/settings.py
@@ -158,3 +158,18 @@ MESSAGE_TAGS = {
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
 }
+
+# Celery
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+
+from celery.schedules import crontab
+CELERY_BEAT_SCHEDULE = {
+    'hello': {
+        'task': 'api.tasks.hello',
+        'schedule': crontab()  # execute every minute
+    }
+}
